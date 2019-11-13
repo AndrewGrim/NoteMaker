8f4a372c35f6daf1f1c9557203c82bd1c86be93e
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2019-11-13 09:06:17 +0000

    now uses 3 tests, better spacing, fixed link and added code support

---
+++ advanced_supported.md
@@ -0,0 +1,52 @@
+# h1 Heading
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+
+## Emphasis
+
+**This is bold text**
+
+*This is italic text*
+
+## Blockquotes
+
+> Blockquotes can also be nested...
+
+## Lists
+
+Unordered
+
++ Create a list by starting a line with `+`, `-`, or `*`
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+## Code
+
+Inline `code`
+
+Indented code
+
+	// Some comments
+	line 1 of code
+	line 2 of code
+	line 3 of code
+
+Syntax highlighting
+
+## Links
+
+[link text](http://dev.nodeca.com)
+
+[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+## Images
+
+![Minion](https://octodex.github.com/images/minion.png)
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
--- src/main.py
+++ src/main.py
@@ -2,174 +2,215 @@
 A very simple and naive markdown parser.
 Reads a file a char at a time and writes the equivalent html to another file.
 Currently supported markdown:
-	all headings 1-6
+	headings
 	bold
 	italic
 	img
+	link
 	blockquote
+	code
 	unordered list(WIP)
 """
-
-r = open("testFile.md", "r")
-w = open("GENERATED_HTML.HTML", "w")
 
 def fileSize(fname):
 	import os
 	statinfo = os.stat(fname)
 	return statinfo.st_size
 
-#w.write(r.read(1))
-#print(fileSize("testFile.md"))
-#r.tell() -> 105
-#r.seek(105)
-fSize = fileSize("testFile.md")
-i = 0
-lineEnd = ""
-ignore = False
-# TODO should probably go back to a line by line read and check only the first index at first, more if needed
-# cause at the moment if i put a hash anywhere in text it will check if its a heading
-# even if its a quote or a coment or whatever
-# TODO add default styling and allow for custom styles
-while r.tell() < fSize:
-	char = r.read(1)
 
-	if ignore:
-		if char == "`":
-			ignore = False
-		else:
-			w.write(char)
-	elif char == "\n":
-		if lineEnd != "":
-			w.write(lineEnd)
-			w.write("\n")
-			lineEnd = ""
-		else:
-			w.write("\n")
-	elif char == "#":
-		hCount = 1
-		while True:
-			c = r.read(1)
-			if c == " ":
-				break
-			elif c == "\n":
-				print("Improperly formatted heading!")
-			hCount += 1
-		if hCount > 6:
-			print("Heading number is too high!")
-		else:
-			w.write(f'<h{hCount}>')
-			if hCount == 1:
-				lineEnd = f"</h{hCount}><hr>"
+files = ["basic", "advanced_supported", "advanced"]
+for f in files: 
+	r = open(f"{f}.md", "r")
+	w = open(f"{f}.html", "w")
+
+	#w.write(r.read(1))
+	#print(fileSize("testFile.md"))
+	#r.tell() -> 105
+	#r.seek(105)
+	fSize = fileSize(f"{f}.md")
+	i = 0
+	lineEnd = ""
+	ignore = False
+	line = 0
+	# TODO should probably go back to a line by line read and check only the first index at first, more if needed
+	# cause at the moment if i put a hash anywhere in text it will check if its a heading
+	# even if its a quote or a coment or whatever
+	# TODO add default styling and allow for custom styles
+	while r.tell() < fSize:
+		char = r.read(1)
+		if char == "\n":
+			line += 1
+
+		if ignore:
+			if char == "`":
+				ignore = False
+				if r.read(1) == "\n":
+					lineEnd += "<br><br>"
+					r.seek(r.tell() - 1)
+				else:
+					r.seek(r.tell() - 1)
 			else:
-				lineEnd = f"</h{hCount}>"
-	elif char == "*" or char == "-":
-		nextC = r.read(1)
-		# TODO this needs to look at the next line since we want to allow numbered lists
-		if nextC == " ":
-			w.write("<ul>\n<li>")
-			lineEnd = "</li>\n</ul>"
-		elif nextC not in ["\n", " ", "*"]:
-			r.seek(r.tell() - 1)
-			c = ""
-			italic = ""
+				w.write(char)
+		elif char == "\n":
+			if lineEnd != "":
+				w.write(lineEnd)
+				lineEnd = ""
+			w.write("\n")
+		elif char == "#":
+			hCount = 1
 			while True:
 				c = r.read(1)
-				if c == "*":
+				if c == " ":
 					break
 				elif c == "\n":
-					print("Closing italic asterisk not found!")
-					break
+					print(f"Line: {line}, TotalChar: {i} -> Improperly formatted heading!")
+				hCount += 1
+			if hCount > 6:
+				print(f"Line: {line}, TotalChar: {i} -> Heading number is too high!")
+			else:
+				w.write(f'<h{hCount}>')
+				if hCount == 1:
+					lineEnd = f"</h{hCount}><hr>"
 				else:
-					italic += c
-			w.write(f"<i>{italic}</i>")
-		elif nextC == "*":
-			c = ""
-			bold = ""
-			while True:
-				c = r.read(1)
-				if c == "*":
-					if r.read(1) == "*":
+					lineEnd = f"</h{hCount}>"
+		elif char in ["*", "-", "+"]: # or char == "-" # or char == "+"
+			nextC = r.read(1)
+			# TODO this needs to look at the next line since we want to allow numbered lists
+			if nextC == " ":
+				w.write("<ul>\n<li>")
+				lineEnd = "</li>\n</ul>"
+			elif nextC not in ["\n", " ", "*"]:
+				r.seek(r.tell() - 1)
+				c = ""
+				italic = ""
+				new = ""
+				while True:
+					c = r.read(1)
+					if c == "*":
+						if r.read(1) == "\n":
+							new = "<br><br>"
+							r.seek(r.tell() - 1)
+							break
+						else:
+							r.seek(r.tell() - 1)
+							break
+					elif c == "\n":
+						print(f"Line: {line}, TotalChar: {i} -> Closing italic asterisk not found!")
 						break
 					else:
-						print("Closing bold asterisk not found!")
-				elif c == "\n":
-					print("Closing bold asterisk not found!")
-					break
-				else:
-					bold += c
-			w.write(f"<b>{bold}</b>")
-		elif nextC == "\n":
-			print("newline following asterisk")
-	elif char == "!":
-		nextC = r.read(1)
-		if nextC == "[":
-			c = ""
-			alt = ""
+						italic += c
+				w.write(f"<i>{italic}</i>{new}")
+			elif nextC == "*":
+				c = ""
+				bold = ""
+				new = ""
+				while True:
+					c = r.read(1)
+					if c == "*":
+						if r.read(1) == "*":
+							if r.read(1) == "\n":
+								new = "<br><br>"
+								r.seek(r.tell() - 1)
+								break
+							else:
+								r.seek(r.tell() - 1)
+								break
+						else:
+							print(f"Line: {line}, TotalChar: {i} -> Closing bold asterisk not found!")
+					elif c == "\n":
+						print(f"Line: {line}, TotalChar: {i} -> Closing bold asterisk not found!")
+						break
+					else:
+						bold += c
+				w.write(f"<b>{bold}</b>{new}")
+			elif nextC == "\n":
+				print(f"Line: {line}, TotalChar: {i} -> newline following asterisk")
+		elif char == "!":
+			nextC = r.read(1)
+			new = ""
+			if nextC == "[":
+				c = ""
+				alt = ""
+				while True:
+					c = r.read(1)
+					if c == "]":
+						break
+					elif c == "\n":
+						print(f"Line: {line}, TotalChar: {i} -> Couldn't find the closing square bracket!")
+						break
+					else:
+						alt += c
+				nextC = r.read(1)
+				if nextC == "(":
+					img = ""
+					while True:
+						c = r.read(1)
+						if c == ")":
+							if r.read(1) == "\n":
+								new = "<br><br>"
+								r.seek(r.tell() - 1)
+								break
+							else:
+								r.seek(r.tell() - 1)
+								break
+						elif c == "\n":
+							print(f"Line: {line}, TotalChar: {i} -> Couldn't find the closing parenthases!")
+							break
+						else:
+							img += c
+					w.write(f'<img src="{img}" alt="{alt}">{new}')
+		elif char == "[":
+			text = ""
+			new = ""
 			while True:
 				c = r.read(1)
 				if c == "]":
 					break
 				elif c == "\n":
-					print("Couldn't find the closing square bracket!")
+					print(f"Line: {line}, TotalChar: {i} -> Couldn't find the closing square bracket!")
 					break
 				else:
-					alt += c
+					text += c
 			nextC = r.read(1)
 			if nextC == "(":
-				img = ""
+				link = ""
 				while True:
 					c = r.read(1)
 					if c == ")":
-						break
+						if r.read(1) == "\n":
+							new = "<br><br>"
+							r.seek(r.tell() - 1)
+							break
+						else:
+							r.seek(r.tell() - 1)
+							break
 					elif c == "\n":
-						print("Couldn't find the closing parenthases!")
+						print(f"Line: {line}, TotalChar: {i} -> Couldn't find the closing parenthases!")
 						break
 					else:
-						img += c
-				w.write(f'<img src="{img}" alt="{alt}">')
-	elif char == "[":
-		text = ""
-		while True:
-			c = r.read(1)
-			if c == "]":
-				break
-			elif c == "\n":
-				print("Couldn't find the closing square bracket!")
-				break
+						link += c
+				w.write(f'<a href="{link}">{text}</a>{new}')
+		elif char == ">": # TODO this is currently bothersome when parsing inline html
+			nextC = r.read(1)
+			if nextC == " ":
+				c = ""
+				block = ""
+				while True:
+					c = r.read(1)
+					if c == "\n":
+						break
+					else:
+						block += c
+				w.write(f"<blockquote>{block}</blockquote>\n")
 			else:
-				text += c
-		nextC = r.read(1)
-		if nextC == "(":
-			link = ""
-			while True:
-				c = r.read(1)
-				if c == ")" or c == "\n":
-					break
-				else:
-					link += c
-			w.write(f'<a href="{link}">{text}</a>')
-	elif char == ">":
-		nextC = r.read(1)
-		if nextC == " ":
-			c = ""
-			block = ""
-			while True:
-				c = r.read(1)
-				if c == "\n":
-					break
-				else:
-					block += c
-			w.write(f"<blockquote>{block}</blockquote>\n")
+				print(f"Line: {line}, TotalChar: {i} -> Improperly formatted blockquote!")
+		elif char == "`":
+			w.write("<code>")
+			lineEnd = "</code>"
+			ignore = True
+		elif char == "<":
+			# start state html tag
+			w.write(char)
 		else:
-			print("Improperly formatted blockquote!")
-	elif char == "`":
-		w.write("<code>")
-		lineEnd = "</code>"
-		ignore = True
-	elif char == "<":
-		# inline html, i could just leave this for else since the end result is the same
-		w.write(char)
-	else:
-		w.write(char)
-	i += 1
-print(i)
+			w.write(char)
+		i += 1
