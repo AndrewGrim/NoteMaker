ad86d1bc37f52960d574c66fd81c8f607bb3a352
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2019-11-19 15:09:37 +0000

    basic parser test

--- src/Parser.py
+++ src/Parser.py
@@ -14,10 +14,49 @@ from Debug import *
 
 LexerToken = NewType('Token', None)
 
+def generateHTML(html: List[str]):
+	try:
+		os.remove("tmp.html")
+	except Exception as e:
+		print(e)
+	f = open("tmp.html", "w")
+	f.write("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Document</title>
+</head>
+	""")
+	style = open("css/default.css", "r").read()
+	f.write(f"<style>\n{style}\n</style>\n")
+	f.write("<body>")
+	f.write('<div class="markdown-body">')
+	for item in html:
+		f.write(item)
+	f.write("</div>")
+	f.write("</body>")
+	f.write("</html>")
+	f.close()
+
+
 def parse(tokens: List[LexerToken]) -> None:
+	html = []
 	i = 0
 	while i < len(tokens):
 		t = tokens[i]
-		if t.id == MD.HTML_BEGIN or t.id == MD.HTML_END:
-			debug(t)
+		if t.id == MD.HEADING:
+			html.append(f"<h{t.end - t.begin - 2}>")
+		elif t.id == MD.HEADING_TEXT:
+			html.append(t.content)
+		elif t.id == MD.HEADING_END:
+			token = None
+			tt = None
+			index = 1
+			while token != MD.HEADING:
+				token = tokens[i - index].id
+				if token == MD.HEADING:
+					tt = tokens[i - index]
+				index += 1
+			html.append(f"</h{tt.end - tt.begin - 2}>")
 		i += 1
+	generateHTML(html)
